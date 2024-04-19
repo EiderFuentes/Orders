4abf13ec5ed548f26b21ed20e802e4cc55b1b185
Eider Fuentes Rivera <97643800+EiderFuentes@users.noreply.github.com>
2024-04-19 10:56:32 -0500

    Login - Logout Service and User register version 1

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Orders.Frontend;
 using Orders.Frontend.AuthenticationProviders;
 using Orders.Frontend.Repositories;
+using Orders.Frontend.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,7 +17,9 @@ builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http
 builder.Services.AddScoped<IRepository, Repository>();//Para configurar la inyeccion de Repository
 builder.Services.AddSweetAlert2();//Inyectamos libreria AddSweetAlert2()
 builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 
+builder.Services.AddScoped<AuthenticationProviderJWT>();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
 
 await builder.Build().RunAsync();

2913b29193274bb31945add22ac8536b79054ec2
Eider Fuentes Rivera <97643800+EiderFuentes@users.noreply.github.com>
2024-05-11 11:39:19 -0500

    Edit product and place images in a carousel

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -3,6 +3,7 @@ using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using Orders.Frontend;
 using Orders.Frontend.AuthenticationProviders;
 using Orders.Frontend.Repositories;
@@ -16,9 +17,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // Estamos haciendo una inyecion AddScoped
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7112/") });
 builder.Services.AddScoped<IRepository, Repository>();//Para configurar la inyeccion de Repository
-builder.Services.AddSweetAlert2();//Inyectamos libreria AddSweetAlert2()
+builder.Services.AddSweetAlert2();//Inyectamos libreria AddSweetAlert2
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredModal();
+builder.Services.AddMudServices();//Inyectamos libreria MudBlazor
 
 builder.Services.AddScoped<AuthenticationProviderJWT>();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
